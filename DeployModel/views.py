c118cee7e46c18f3eb7019a5b26df07d87ef56ea
Pranav <pranavv2002@gmail.com>
2024-03-02 11:13:55 +0530

    Supabase

--- DeployModel/views.py
+++ DeployModel/views.py
@@ -7,8 +7,8 @@ import pprint
 # import os
 
 
-supabase_url = 'https://jbxwmefylhjinddezouk.supabase.co'
-supabase_api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpieHdtZWZ5bGhqaW5kZGV6b3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1NzE5NjcsImV4cCI6MjAyNDE0Nzk2N30.-F5-r5UZDXUvSB-UawcQQxkHoo7E3qqnxrtRTxLM41Y'
+supabase_url = 'https://hotqztzexkqnxtrfeany.supabase.co'
+supabase_api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvdHF6dHpleGtxbnh0cmZlYW55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkzNTcyMzcsImV4cCI6MjAyNDkzMzIzN30.nWHwuaAN69VPLGOwaGUqQWVDed85anHx0xB-CE30y78'
 
 supabase = create_client(supabase_url, supabase_api_key)
 
@@ -38,7 +38,7 @@ def illicit_page(request):
         txid = request.GET['txid']
         txid_data = get_transaction_data(txid)
 
-        # vin, vout = txid_data['vin_sz'], txid_data['vout_sz']
+        vin, vout = txid_data['vin_sz'], txid_data['vout_sz']
         # print(type(txid_data['inputs']))
         bin,bout=0,0
         for i in txid_data['inputs']:
@@ -51,12 +51,12 @@ def illicit_page(request):
         print(bin, bout)
 
         # print(vin, vout)
-
         model = joblib.load('illicit_model_v001.sav')
 
         model_result = model.predict([[4, 3, 4.939574, 5.788070]])[0]
         # print(model_result)
-
+        data, count = supabase.table('Illicit').insert({"tx_id": txid, "vin": vin, "vout": vout, "bin": bin, "bout": bout, "illicit": model_result}).execute()
+        print(data)
         # supabase txid, vin, vout, bin, bout, 
         return render(request, "illicit.html", {'vin': vin, 'vout': vout, 'illicit': model_result})
 
